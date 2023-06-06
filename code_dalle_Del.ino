9366a19e4803bf35f850b4a1894e19f033513322
Loïck <116193668+e0289033@users.noreply.github.com>
2023-06-06 14:59:27 -0400

    Update code_dalle_Del.ino

--- code_dalle_Del.ino
+++ code_dalle_Del.ino
@@ -16,27 +16,65 @@
 // create a display object of type TM1637Display
 TM1637Display display = TM1637Display(CLK, DIO);
 
-ezButton limitSwitchA(7);  // create ezButton object that attach to pin 7;
-ezButton limitSwitchB(4);  // create ezButton object that attach to pin 7;
+const int ledPin1A =  13;
+const int ledPin2A =  13; 
+const int ledPin3B =  13; 
+const int ledPin4B =  13; 
+
+int ledStateA = LOW;
+int ledStateB = LOW;
+
+int slidePin = 0;
+
+int slideState = LOW;
+
+
+long previousMillis = 0;
+
+long ledInterval = 1000;
+
+ezButton limitSwitchA(7);  // create limitSwitch object that attach to pin 7
 
 int points = 0;
 const int limitSwitchAPointBonus = 1;
-const int limitSwitchBPointBonus = 5;
+const int slidePointBonus = 10;
 
 
 void setup() {
+  pinMode(slidePin, INPUT_PULLUP);
+
+  pinMode(ledPin1A, OUTPUT);
+  pinMode(ledPin2A, OUTPUT);
+  pinMode(ledPin3B, OUTPUT);
+  pinMode(ledPin4B, OUTPUT);
+
   display.clear();
   display.setBrightness(7);
   Serial.begin(9600);
-  limitSwitchA.setDebounceTime(0); // set debounce time to 50 milliseconds
   limitSwitchA.setDebounceTime(0); // set debounce time to 50 milliseconds
 }
 
 
 void loop() {
+  unsigned long currentMillis = millis();
   
+  slideState = analogRead(slidePin);
+
+  if(currentMillis - previousMillis > ledInterval){
+    previousMillis = currentMillis;
+
+    if(ledStateA == LOW){
+      ledStateA = HIGH;
+      ledStateB = LOW;
+    }else{
+      ledStateA = LOW;
+      ledStateB = HIGH;
+    }
+
+    updateLeds();
+  }
+
   limitSwitchA.loop(); // MUST call the loop() function first
-  limitSwitchB.loop(); // MUST call the loop() function first
 
   UpdatePoints();
   display.showNumberDec(points);
@@ -47,12 +85,16 @@ void UpdatePoints()
     //limitSwitchA check
     if(limitSwitchA.isPressed()){
       display.clear();
-      points = points + limitSwitchAPointBonus;
-  }
+      points + limitSwitchAPointBonus;
+    }
+    if(slideState == HIGH){
+      points + slidePointBonus;
+    }
+}
 
-    //limitSwitchB check
-    if(limitSwitchB.isPressed()){
-      display.clear();
-      points = points + limitSwitchBPointBonus;
-  }
+void updateLeds(){
+  digitalWrite(ledPin1A,ledStateA);
+  digitalWrite(ledPin2A,ledStateA);
+  digitalWrite(ledPin3B,ledStateB);
+  digitalWrite(ledPin4B,ledStateB);
 }
